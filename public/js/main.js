12a8e9fc804e90bc5769d932a521b8c0cfb6aec8
jhmartinez <jessica.hdez.martinez@gmail.com>
2018-02-09 01:34:16 +0100

    Seccion proposiciones de leyes

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
     console.log('Documento listo');
     init();
 
@@ -14,6 +16,8 @@ $(document).ready(function() {
 var init = function() {
     carousel();
     manageDropdown();
+    manageSttars();
+    collapseAside();
 };
 
 
@@ -41,4 +45,36 @@ var carousel = function () {
     $("#nextBtn").click(function(){
         $("#myCarouselCustom").carousel("next");
     });
-}
+};
+
+var manageSttars = function () {
+    $('.li-estrellas span').on({
+        mouseover: function (e) {
+            $(this).removeClass('glyphicon-star-empty').prevAll().removeClass('glyphicon-star-empty');
+            $(this).addClass('glyphicon-star').prevAll().addClass('glyphicon-star');
+        },
+        mouseleave: function () {
+            $(this).removeClass('glyphicon-star').prevAll().removeClass('glyphicon-star');
+            $(this).addClass('glyphicon-star-empty').prevAll().addClass('glyphicon-star-empty');
+        },
+        click: function () {
+            $(this).removeClass('glyphicon-star-empty').prevAll().removeClass('glyphicon-star-empty');
+            $(this).addClass('glyphicon-star').prevAll().addClass('glyphicon-star');
+            $('.li-estrellas span').off();
+        }
+
+    });
+};
+
+var collapseAside = function () {
+
+    $('.item-aside-collapse').on({
+        'show.bs.collapse': function () {
+            $(this).children().first().removeClass('glyphicon-menu-right').addClass('glyphicon-menu-down');
+        },
+        'hidden.bs.collapse': function () {
+            $(this).children().first().removeClass('glyphicon-menu-down').addClass('glyphicon-menu-right');
+        }
+
+    });
+};
